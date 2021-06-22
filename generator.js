c8ddf3e785b5f84dce202c215524dc036c5b3b29
GameKyuubi <xtra.zero@gmail.com>
2021-06-22 00:31:21 -0700

    get all relevant paths

--- generator.js
+++ generator.js
@@ -1,4 +1,8 @@
 var fs = require("fs");
 
-let inputPath = process.argv[2];
-console.log(inputPath);
+let videoPath = process.argv[2];
+let logDir = (process.argv[3]) ? process.argv[3] : '.';
+let outputPath = (process.argv[4]) ? process.argv[4] : 'output.txt';
+console.log("videoPath: %s", videoPath);
+console.log("logDir: %s", logDir);
+console.log("outputPath: %s", outputPath);
